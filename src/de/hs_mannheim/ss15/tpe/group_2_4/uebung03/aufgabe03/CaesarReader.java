c7ef83f0d0b2500663784e44dfee410edd16b077
pi <pi@141.19.167.126>
2015-05-12 12:50:30 +0200

    Caesar Writer/Reader: alle unnötigen Methoden gelöscht, Fehler verbessert

--- src/de/hs_mannheim/ss15/tpe/group_2_4/uebung03/aufgabe03/CaesarReader.java
+++ src/de/hs_mannheim/ss15/tpe/group_2_4/uebung03/aufgabe03/CaesarReader.java
@@ -9,17 +9,12 @@ import java.io.*;
 public class CaesarReader extends FilterReader {
 
     private int key;
-        
-    @Override
-    public void close() throws IOException {
-        super.close(); //To change body of generated methods, choose Tools | Templates.
+     
+    protected CaesarReader(Reader in) {
+        super(in);
+        this.in = in;
     }
-
-    @Override
-    public void reset() throws IOException {
-        super.reset(); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    
     @Override
     public void mark(int readAheadLimit) throws IOException {
         super.mark(readAheadLimit); //To change body of generated methods, choose Tools | Templates.
@@ -31,16 +26,6 @@ public class CaesarReader extends FilterReader {
     }
 
     @Override
-    public boolean ready() throws IOException {
-        return super.ready(); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public long skip(long n) throws IOException {
-        return super.skip(n); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
     public int read(char[] cbuf, int off, int len) throws IOException {
         return super.read(cbuf, off, len); //To change body of generated methods, choose Tools | Templates.
     }
@@ -48,6 +33,26 @@ public class CaesarReader extends FilterReader {
     @Override
     public int read() throws IOException {
         return super.read(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String decrypt(String cypherText) {
+            String message = "";
+
+            while(cypherText.length() > 0) {
+                    char h = cypherText.charAt(0);
+                    //Only a-z and A-Z
+                    if((('a' <= h)  && (h <= 'z')) || (('A' <= h) && (h <= 'Z'))) {
+                            if((('a' <= h)  && (h <= 'z')))
+                                    h = (char)(h - 'a'); //See encrypt()
+                            else
+                                    h = (char)(h - 'A');
+                            h = (char)((h - key + 26) % 26); //+26 so modulo does work (somehow doesn't with negative ints)
+                            h = (char)(h + 'A');
+                    }
+                    message += h;
+                    cypherText = cypherText.substring(1, cypherText.length());
+            }
+            return message;
     }
     
 }
--- src/de/hs_mannheim/ss15/tpe/group_2_4/uebung03/aufgabe03/CaesarWriter.java
+++ src/de/hs_mannheim/ss15/tpe/group_2_4/uebung03/aufgabe03/CaesarWriter.java
@@ -11,18 +11,9 @@ public class CaesarWriter extends FilterWriter {
     /**
      *
      */
-    public CaesarWriter() {
-        
-    }
-        
-    @Override
-    public void close() throws IOException {
-        super.close(); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void flush() throws IOException {
-        super.flush(); //To change body of generated methods, choose Tools | Templates.
+    protected CaesarWriter(Writer out) {
+        super(out);
+        this.out = out;
     }
 
     @Override
@@ -39,5 +30,25 @@ public class CaesarWriter extends FilterWriter {
     public void write(int c) throws IOException {
         super.write(c); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    private String encrypt(String message) {
+            String cypherText = "";
+
+            //Encryption by shifting all letters by given key to the right.
+            while(message.length() > 0) {
+                    char h = message.charAt(0);
+                    //Only a-z and A-Z
+                    if((('a' <= h)  && (h <= 'z')) || (('A' <= h) && (h <= 'Z'))) {
+                            if((('a' <= h)  && (h <= 'z')))
+                                    h = (char)(h - 'a'); //Number ranges from 0 - 25...
+                            else
+                                    h = (char)(h - 'A');
+                            h = (char)((h + key) % 26); //...so it can be operated with modulo (rollover detection)
+                            h = (char)(h + 'A'); //Also make everything uppercase
+                    }
+                    cypherText += h;
+                    message = message.substring(1, message.length());
+            }
+            return cypherText;
+    }
 }
