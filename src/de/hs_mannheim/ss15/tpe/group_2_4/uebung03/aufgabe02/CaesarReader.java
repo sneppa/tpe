c2889f94a8d35ba445f3c2823997dfc4fbd084aa
pi <pi@piquadrat>
2015-05-25 21:18:51 +0200

    Aufgabe 2 fertig

--- src/de/hs_mannheim/ss15/tpe/group_2_4/uebung03/aufgabe02/CaesarReader.java
+++ src/de/hs_mannheim/ss15/tpe/group_2_4/uebung03/aufgabe02/CaesarReader.java
@@ -1,5 +1,6 @@
-
 package de.hs_mannheim.ss15.tpe.group_2_4.uebung03.aufgabe02;
+
+import static gdi.MakeItSimple.*;
 import java.io.*;
 
 /**
@@ -9,56 +10,26 @@ import java.io.*;
 public class CaesarReader extends FilterReader {
 
     private int key;
-     
+    private CaesarCrypter crypter;
+
+    /**
+     * Initialize the FilterReader
+     * @param in
+     * @param key CaesarKey
+     */
     public CaesarReader(Reader in, int key) {
         super(in);
+        crypter = new CaesarCrypter(key);
         this.in = in;
         this.key = key;
     }
-     
-    protected CaesarReader(Reader in) {
-        super(in);
-        this.in = in;
-    }
-    
-    @Override
-    public void mark(int readAheadLimit) throws IOException {
-        super.mark(readAheadLimit); //To change body of generated methods, choose Tools | Templates.
-    }
 
-    @Override
-    public boolean markSupported() {
-        return super.markSupported(); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int read(char[] cbuf, int off, int len) throws IOException {
-        return super.read(cbuf, off, len); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
+    /**
+     * This method overrides the read() from the superclass and realizes the caesar-decoding
+     */
     public int read() throws IOException {
-        return super.read(); //To change body of generated methods, choose Tools | Templates.
+        char h = (char) super.read();
+//        println(h);
+        return crypter.decrypt(h+"").toCharArray()[0];
     }
-    
-    private String decrypt(String cypherText) {
-            String message = "";
-
-            while(cypherText.length() > 0) {
-                    char h = cypherText.charAt(0);
-                    //Only a-z and A-Z
-                    if((('a' <= h)  && (h <= 'z')) || (('A' <= h) && (h <= 'Z'))) {
-                            if((('a' <= h)  && (h <= 'z')))
-                                    h = (char)(h - 'a'); //See encrypt()
-                            else
-                                    h = (char)(h - 'A');
-                            h = (char)((h - key + 26) % 26); //+26 so modulo does work (somehow doesn't with negative ints)
-                            h = (char)(h + 'A');
-                    }
-                    message += h;
-                    cypherText = cypherText.substring(1, cypherText.length());
-            }
-            return message;
-    }
-    
 }
--- src/de/hs_mannheim/ss15/tpe/group_2_4/uebung03/aufgabe02/CaesarWriter.java
+++ src/de/hs_mannheim/ss15/tpe/group_2_4/uebung03/aufgabe02/CaesarWriter.java
@@ -1,5 +1,7 @@
 package de.hs_mannheim.ss15.tpe.group_2_4.uebung03.aufgabe02;
+
 import java.io.*;
+
 /**
  *
  * @author pi
@@ -7,51 +9,34 @@ import java.io.*;
 public class CaesarWriter extends FilterWriter {
 
     private int key;
-    
+    private CaesarCrypter crypter;
+
+    /**
+     * Initialize the Writer
+     * @param out
+     * @param key Caesar Key
+     */
     public CaesarWriter(Writer out, int key) {
         super(out);
+        crypter = new CaesarCrypter(key);
         this.out = out;
         this.key = key;
-    }
-    
-    protected CaesarWriter(Writer out) {
-        super(out);
-        this.out = out;
     }
 
     @Override
     public void write(String str, int off, int len) throws IOException {
-        super.write(str, off, len); //To change body of generated methods, choose Tools | Templates.
+        write(str.toCharArray(), off, len); // String -> CharArray -> write(chararray)
     }
 
     @Override
     public void write(char[] cbuf, int off, int len) throws IOException {
-        super.write(cbuf, off, len); //To change body of generated methods, choose Tools | Templates.
+        for (int i = 0; i < len; ++i) {
+            write(cbuf[off + i]); // Pass every char from the char array to the write(int)
+        }
     }
 
     @Override
     public void write(int c) throws IOException {
-        super.write(c); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private String encrypt(String message) {
-            String cypherText = "";
-
-            //Encryption by shifting all letters by given key to the right.
-            while(message.length() > 0) {
-                    char h = message.charAt(0);
-                    //Only a-z and A-Z
-                    if((('a' <= h)  && (h <= 'z')) || (('A' <= h) && (h <= 'Z'))) {
-                            if((('a' <= h)  && (h <= 'z')))
-                                    h = (char)(h - 'a'); //Number ranges from 0 - 25...
-                            else
-                                    h = (char)(h - 'A');
-                            h = (char)((h + key) % 26); //...so it can be operated with modulo (rollover detection)
-                            h = (char)(h + 'A'); //Also make everything uppercase
-                    }
-                    cypherText += h;
-                    message = message.substring(1, message.length());
-            }
-            return cypherText;
+        super.write(crypter.encrypt(((char) c)+"").toCharArray()[0]); // Encrypt the char and write it to the file
     }
 }
